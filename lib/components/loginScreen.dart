a3be9ba983212d5d084f76f7d032b5483368a3f0
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-01-02 21:31:14 +0530

    Add login screen empty

---
+++ lib/components/loginScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<LoginScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
