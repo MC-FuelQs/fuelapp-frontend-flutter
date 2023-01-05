b1da71b57ca4205f3f6ba3add7bd4ea59d495350
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-01-06 05:15:44 +0530

    Add left queue button functons and store left time in database

--- lib/components/stationPage.dart
+++ lib/components/stationPage.dart
@@ -26,7 +26,9 @@ class _StationPageState extends State<StationPage> {
   String _selectedVehicleType = "Petrol";
   List vehicalTypes = ["Petrol", "Diesel"];
   int joinedTime = 0;
+  int leftTime = 0;
   bool joinedToQueue = false;
+  bool leftQueue = false;
   String feedId = '';
 
   @override
@@ -79,6 +81,25 @@ class _StationPageState extends State<StationPage> {
     }
   }
 
+  leaveQueue(String feedId, int leftTime) async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString('authToken');
+
+    var url = '$API_URL/api/feed/$feedId';
+    var response = await http.put(
+      Uri.parse(url),
+      headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+        HttpHeaders.authorizationHeader: "Bearer $token"
+      },
+      body: jsonEncode(<String, String>{
+        'departTime': leftTime.toString(),
+      }),
+    );
+    // ignore: avoid_print
+    print('left Q status: ${response.statusCode}');
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -88,87 +109,139 @@ class _StationPageState extends State<StationPage> {
         body: Container(
           color: Colors.brown.shade200,
           padding: const EdgeInsets.all(30),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: DropdownButtonFormField<String>(
-                    hint: const Text('Select a Station'),
-                    value: _selectedStation,
-                    items: sheds.map((item) {
-                      return new DropdownMenuItem(
-                        value: item['_id'].toString(),
-                        child: new Text(
-                          item['shedName'],
-                          style: const TextStyle(
-                            color: Colors.white,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: DropdownButtonFormField<String>(
+                hint: const Text('Select a Station'),
+                value: _selectedStation,
+                items: sheds.map((item) {
+                  return new DropdownMenuItem(
+                    value: item['_id'].toString(),
+                    child: new Text(
+                      item['shedName'],
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedStation = newValue.toString();
+                  });
+                },
+                icon: const Icon(Icons.arrow_drop_down_circle_outlined,
+                    color: Colors.brown),
+                dropdownColor: Colors.brown,
+                decoration: const InputDecoration(
+                  labelText: "Select a Station",
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: DropdownButtonFormField<String>(
+                hint: const Text('Select your vehical type'),
+                value: _selectedVehicleType,
+                items: vehicalTypes.map((item) {
+                  return new DropdownMenuItem(
+                    value: item.toString(),
+                    child: new Text(
+                      item,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedVehicleType = newValue.toString();
+                  });
+                },
+                icon: const Icon(Icons.arrow_drop_down_circle_outlined,
+                    color: Colors.brown),
+                dropdownColor: Colors.brown,
+                decoration: const InputDecoration(
+                  labelText: "Select Vehical Type",
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: !joinedToQueue
+                  ? Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          joinedTime = DateTime.now().millisecondsSinceEpoch;
+                          setState(() {
+                            joinedToQueue = true;
+                          });
+                          joinQueue(_selectedStation, _selectedVehicleType,
+                              joinedTime);
+                          print("Joined to queue");
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                              color: Colors.brown.shade600,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Center(
+                            child: Text(
+                              'Join the queue',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
                           ),
                         ),
-                      );
-                    }).toList(),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedStation = newValue.toString();
-                      });
-                    },
-                    icon: const Icon(Icons.arrow_drop_down_circle_outlined,
-                        color: Colors.brown),
-                    dropdownColor: Colors.brown,
-                    decoration: const InputDecoration(
-                      labelText: "Select a Station",
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: DropdownButtonFormField<String>(
-                    hint: const Text('Select your vehical type'),
-                    value: _selectedVehicleType,
-                    items: vehicalTypes.map((item) {
-                      return new DropdownMenuItem(
-                        value: item.toString(),
-                        child: new Text(
-                          item,
-                          style: const TextStyle(
-                            color: Colors.white,
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                            color: Colors.brown.shade200,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: const Center(
+                          child: Text(
+                            'Joined',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
                           ),
                         ),
-                      );
-                    }).toList(),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedVehicleType = newValue.toString();
-                      });
-                    },
-                    icon: const Icon(Icons.arrow_drop_down_circle_outlined,
-                        color: Colors.brown),
-                    dropdownColor: Colors.brown,
-                    decoration: const InputDecoration(
-                      labelText: "Select Vehical Type",
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  child: !joinedToQueue
-                      ? Padding(
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: !leftQueue && joinedToQueue
+                  ? Column(
+                      children: [
+                        Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 25.0),
                           child: GestureDetector(
                             onTap: () {
-                              joinedTime =
-                                  DateTime.now().millisecondsSinceEpoch;
                               setState(() {
-                                joinedToQueue = true;
+                                leftQueue = true;
+                                leftTime =
+                                    DateTime.now().millisecondsSinceEpoch;
+                                leaveQueue(feedId, leftTime);
                               });
-                              joinQueue(_selectedStation, _selectedVehicleType,
-                                  joinedTime);
-                              print("Joined to queue");
                             },
                             child: Container(
                               padding: const EdgeInsets.all(20.0),
@@ -177,7 +250,7 @@ class _StationPageState extends State<StationPage> {
                                   borderRadius: BorderRadius.circular(12)),
                               child: const Center(
                                 child: Text(
-                                  'Join the queue',
+                                  'Exit Before Pump',
                                   style: TextStyle(
                                       color: Colors.white,
                                       fontWeight: FontWeight.bold,
@@ -186,94 +259,76 @@ class _StationPageState extends State<StationPage> {
                               ),
                             ),
                           ),
-                        )
-                      : Container(
-                          child: Padding(
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Container(
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                                color: Colors.brown.shade200,
-                                borderRadius: BorderRadius.circular(12)),
-                            child: const Center(
-                              child: Text(
-                                'Joined',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                leftQueue = true;
+                                leftTime =
+                                    DateTime.now().millisecondsSinceEpoch;
+                                leaveQueue(feedId, leftTime);
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.brown.shade600,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: const Center(
+                                child: Text(
+                                  'Exit After Pump',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
                               ),
                             ),
                           ),
-                        )),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return const HomePage();
-                          },
                         ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                          color: Colors.brown.shade600,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: const Center(
-                        child: Text(
-                          'Exit Before Pump',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ),
+                      ],
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: joinedToQueue
+                          ? Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.brown.shade200,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: const Center(
+                                child: Text(
+                                  'Left the Queue',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                              ),
+                            )
+                          : Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.brown.shade200,
+                                  borderRadius: BorderRadius.circular(12)),
+                              child: const Center(
+                                child: Text(
+                                  'Join before leave',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                              ),
+                            ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return const HomePage();
-                          },
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                          color: Colors.brown.shade600,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: const Center(
-                        child: Text(
-                          'Exit After Pump',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
+            ),
+          ]),
         ));
   }
 }
