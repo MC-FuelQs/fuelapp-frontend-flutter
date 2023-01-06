8085d62b9db014ad5e3bfb0cf0989c3e5ec1337f
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-01-07 00:54:50 +0530

    Add dialog box for login

--- lib/components/loginScreen.dart
+++ lib/components/loginScreen.dart
@@ -77,15 +77,10 @@ class _LoginScreenState extends State<LoginScreen> {
       var res_body = json.decode(response.body);
       storeUserToken(res_body['token']);
       storeUserName(username);
-      if (selectedUserType == 'Filling Station Owner') {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => const MySheds()));
-      } else if (selectedUserType == 'Vehical Owner') {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => const HomePage()));
-      }
+      loginSuccessDialog(selectedUserType);
     } else {
       print("Failed login");
+      loginFailedDialog();
     }
   }
 
@@ -95,166 +90,230 @@ class _LoginScreenState extends State<LoginScreen> {
         body: SingleChildScrollView(
           child: SafeArea(
             child: Center(
-              child:
-                  Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                // welcome back
-                const Icon(
-                  Icons.directions_car_filled,
-                  size: 150,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Login",
-                  style: GoogleFonts.bebasNeue(
-                    fontSize: 52,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Welcome back user!",
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 50,
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: DropdownButtonFormField(
-                    value: _selectedUserType,
-                    items: _UserType.map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Text(e),
-                        )).toList(),
-                    onChanged: (val) {
-                      setState(() {
-                        _selectedUserType = val as String;
-                      });
-                    },
-                    icon: const Icon(Icons.arrow_drop_down_circle_outlined,
-                        color: Colors.brown),
-                    dropdownColor: Colors.brown[50],
-                    decoration: const InputDecoration(
-                        labelText: "Vehical Type",
-                        prefixIcon: Icon(Icons.supervised_user_circle)),
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-
-                //username
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12)),
-                    child: TextField(
-                      onChanged: (val) {
-                        setState(() {
-                          username = val;
-                        });
-                      },
-                      decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Enter Your Username',
-                          labelText: 'Username',
-                          prefixIcon: Icon(Icons.person_rounded)),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // welcome back
+                    const Icon(
+                      Icons.directions_car_filled,
+                      size: 150,
                     ),
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-                //password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12)),
-                    child: TextField(
-                      onChanged: (val) {
-                        setState(() {
-                          password = val;
-                        });
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Enter your Password',
-                        labelText: 'Password',
-                        prefixIcon: Icon(Icons.password_rounded),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Login",
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 52,
                       ),
                     ),
-                  ),
-                ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Welcome back user!",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
 
-                const SizedBox(
-                  height: 10,
-                ),
+                    const SizedBox(
+                      height: 50,
+                    ),
 
-                // signin button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      authenticateUser(username, password, _selectedUserType);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                          color: Colors.brown.shade600,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: const Center(
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: DropdownButtonFormField(
+                        value: _selectedUserType,
+                        items: _UserType.map((e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(e),
+                            )).toList(),
+                        onChanged: (val) {
+                          setState(() {
+                            _selectedUserType = val as String;
+                          });
+                        },
+                        icon: const Icon(Icons.arrow_drop_down_circle_outlined,
+                            color: Colors.brown),
+                        dropdownColor: Colors.brown[50],
+                        decoration: const InputDecoration(
+                            labelText: "Vehical Type",
+                            prefixIcon: Icon(Icons.supervised_user_circle)),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    //username
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: TextField(
+                          onChanged: (val) {
+                            setState(() {
+                              username = val;
+                            });
+                          },
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter Your Username',
+                              labelText: 'Username',
+                              prefixIcon: Icon(Icons.person_rounded)),
                         ),
                       ),
                     ),
-                  ),
-                ),
 
-                const SizedBox(
-                  height: 20,
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Not a member?'),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => const RegisterScreen()));
-                      },
-                      child: Text(
-                        ' Register',
-                        style: TextStyle(color: Colors.blue.shade600),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    //password
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: TextField(
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter your Password',
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.password_rounded),
+                          ),
+                        ),
                       ),
                     ),
-                  ],
-                )
-              ]),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    // signin button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          authenticateUser(
+                              username, password, _selectedUserType);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                              color: Colors.brown.shade600,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Center(
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Not a member?'),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RegisterScreen()));
+                          },
+                          child: Text(
+                            ' Register',
+                            style: TextStyle(color: Colors.blue.shade600),
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
             ),
           ),
-        )
+        ));
+  }
+
+  Future<void> loginFailedDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Login failed'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Please check your credentials'),
+                Text('Or Register'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Okay'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> loginSuccessDialog(selectedUserType) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Login Success'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Press okay to continue'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Okay'),
+              onPressed: () {
+                if (selectedUserType == 'Filling Station Owner') {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const MySheds()));
+                } else if (selectedUserType == 'Vehical Owner') {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const HomePage()));
+                }
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
