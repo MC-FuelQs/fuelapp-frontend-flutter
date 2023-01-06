7308e8f7cbd47b8309bf76844d0a8e72af263b02
Nuwan-Wi <nuwanwijeweera@gmail.com>
2023-01-06 21:40:48 +0530

    Splash Scren added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:fuel_app/components/Splash.dart';
 import 'package:fuel_app/components/loginScreen.dart';
 import 'package:fuel_app/components/mySheds.dart';
 import 'package:fuel_app/components/registerScreen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: Splash(),
     );
   }
 }
