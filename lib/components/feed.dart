de1bfb2053cc6f59f6e3eae2d8e6c23e10f665ed
Chathuranga97lj <chathuit97@gmail.com>
2023-01-06 12:28:22 +0530

    seperate feed widget

--- lib/components/feed.dart
+++ lib/components/feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fuel_app/components/widgets/feedHeader.dart';
 
 class Feed extends StatefulWidget {
   const Feed({Key? key}) : super(key: key);
@@ -19,55 +20,7 @@ class _FeedState extends State<Feed> {
           leading: Icon(Icons.store_outlined)),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Container(
-                  height: 70,
-                  decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.only(bottomLeft: Radius.circular(20)),
-                      color: Colors.purpleAccent),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(Icons.warehouse_outlined),
-                      Text('Fuel Station')
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                color: Colors.blueAccent,
-                height: 70,
-                width: 70,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [Icon(Icons.car_repair), Text('Petorl')],
-                ),
-              ),
-              Container(
-                  color: Colors.pinkAccent,
-                  height: 70,
-                  width: 70,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [Icon(Icons.fire_truck_sharp), Text('Desel')],
-                  )),
-              Container(
-                  height: 70,
-                  width: 70,
-                  decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.only(bottomRight: Radius.circular(20)),
-                      color: Colors.purpleAccent),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [Icon(Icons.timelapse_outlined), Text('Waiting')],
-                  ))
-            ],
-          ),
+          FeedHeader(),
         ],
       ),
     );
